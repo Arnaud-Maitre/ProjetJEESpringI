d8b4da97386d8af335126dd872c878b94094b281
Arnaud <arnaud.maitre@he-arc.ch>
2024-12-11 10:26:01 +0100

    Initial commit

---
+++ src/main/java/ch/hearc/jee2024/projetjeespring/ProjetJEESpringApplication.java
@@ -0,0 +1,13 @@
+package ch.hearc.jee2024.projetjeespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJEESpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetJEESpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/hearc/jee2024/projetjeespring/ProjetJEESpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.hearc.jee2024.projetjeespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJEESpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
